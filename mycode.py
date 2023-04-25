146b368300b4de7ad95aa78d9233d89e654a8531
Anastasia Kudzeshvili <a.kudzeshvili@gmail.com>
2023-04-25 23:03:52 +0200

    add argparse

--- mycode.py
+++ mycode.py
@@ -2,13 +2,22 @@ import re
 import csv
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", default="./logs/ssl-access.log.2", help="Path to input file")
+parser.add_argument("-o", "--output", default="./outputs/output.csv", help="Path to output file")
+args = parser.parse_args()
+
 pattern = r"/root_v(\d+\.\d+\.\d+)\.(Linux|macos|win32|win64|source)"
 
 data = []
 
-with open("./logs/ssl-access.log.2", "r") as log_file:
+print("Using input file:", args.input)
+print("Using output file:", args.output)
+
+
+with open(args.input, "r") as log_file:
     for line in log_file:
-        if 'GET' in line:
+        if 'GET' in line: 
             match = re.search(pattern, line)
             if match:
                 version = match.group(1)
@@ -18,7 +27,7 @@ with open("./logs/ssl-access.log.2", "r") as log_file:
                 if 'bot' not in user_agent.lower() and 'spider' not in user_agent.lower() and 'crawl' not in user_agent.lower():
                     data.append([date, version, platform])
 
-with open("./outputs/output.csv", "w") as output_file:
+with open(args.output, "w") as output_file:
     writer = csv.writer(output_file)
     writer.writerow(["Date", "Version", "Platform"])
     writer.writerows(data)
